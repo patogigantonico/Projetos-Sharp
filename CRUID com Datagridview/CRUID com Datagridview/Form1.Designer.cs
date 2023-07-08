8d4ec7b6840bb193a86b12093130185a7f8ed564
Dan <131774703+patogigantonico@users.noreply.github.com>
2023-07-08 19:14:27 -0300

    Programa com CRUD simples
    Este simples programa Cria, deleta e atualiza informações inseridas em uma tabela do Datagridview.

---
+++ CRUID com Datagridview/CRUID com Datagridview/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace CRUID_com_Datagridview
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.rectangleShape1 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.rectangleShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(573, 279);
+            this.shapeContainer1.TabIndex = 7;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // rectangleShape1
+            // 
+            this.rectangleShape1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.rectangleShape1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.rectangleShape1.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.rectangleShape1.Location = new System.Drawing.Point(6, 188);
+            this.rectangleShape1.Name = "rectangleShape1";
+            this.rectangleShape1.Size = new System.Drawing.Size(557, 83);
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(6, 13);
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.Size = new System.Drawing.Size(558, 167);
+            this.dgv.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 219);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Nome";
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnAdicionar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnAdicionar.BackgroundImage")));
+            this.btnAdicionar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnAdicionar.Location = new System.Drawing.Point(310, 235);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(32, 32);
+            this.btnAdicionar.TabIndex = 10;
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.txtNome.Location = new System.Drawing.Point(12, 235);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(143, 32);
+            this.txtNome.TabIndex = 11;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.txtEmail.Location = new System.Drawing.Point(161, 235);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(143, 32);
+            this.txtEmail.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(158, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "E-mail";
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnRemover.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnRemover.BackgroundImage")));
+            this.btnRemover.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnRemover.Location = new System.Drawing.Point(386, 235);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(32, 32);
+            this.btnRemover.TabIndex = 14;
+            this.btnRemover.UseVisualStyleBackColor = true;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnAtualizar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnAtualizar.BackgroundImage")));
+            this.btnAtualizar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnAtualizar.Location = new System.Drawing.Point(348, 235);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(32, 32);
+            this.btnAtualizar.TabIndex = 15;
+            this.btnAtualizar.UseVisualStyleBackColor = true;
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 279);
+            this.Controls.Add(this.btnAtualizar);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.shapeContainer1);
+            this.Name = "Form1";
+            this.Text = "CRUD Datagridview";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape1;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.Button btnAtualizar;
+    }
+}
+
---
+++ CRUID com Datagridview/CRUID com Datagridview/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
+
+namespace CRUID_com_Datagridview
+{
+    public partial class Form1 : Form
+    {
+        //Instância da classe datagridview no objeto dt.
+        DataTable dt = new DataTable();
+        int id = 1; //Variável que recebe o valor do id. 
+
+        //Sessão de métodos//
+
+        private void CarregarTabela() //Este método faz com que as colunas sejam criadas no datagridview.
+        {
+            dt.Columns.Add("id", typeof(Int32)); //A coluna id do tipo inteiro 32 é adicionada.
+            dt.Columns["id"].AutoIncrement = true; //A coluna id é definida como autoincremento, ou seja seu valor é adicionado automaticamente.
+            dt.Columns["id"].AutoIncrementSeed = 1; //A coluna id recebe um incremento de +1  a cada vez que for adicionado.
+
+            dt.Columns.Add("nome", typeof(string));//A nome id do tipo string é adicionada.
+            dt.Columns.Add("e-mail", typeof(string));//A coluna e-mail do tipo string é adicionada.
+            dt.Columns.Add("Classificação", typeof(string));//A coluna Classificação do tipo string é adicionada.
+
+            dgv.DataSource = dt; //O datagridview recebe todas as colunas que foram definidas na instância dt.
+        }
+
+        private void RemoverLinha()
+        {
+            //Se a conta de linhas for 0, ou seja se não houver nenhuma linha no datagrid, mostrar a mensagem
+            if (dgv.Rows.Count == 0)
+            {
+                MessageBox.Show("Não há nenhum dado na tabela", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            //Se nenhuma linha estiver selecionada, mostrar a mensagem.
+            else if (dgv.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Selecione uma linha inteira primeiro!", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (dgv.SelectedRows.Count > 0) // Se houver pelo menos uma linha selecionada (mas não mais do que uma), remover a linha selecionada.
+            {
+                foreach (DataGridViewRow row in dgv.SelectedRows)
+                {
+                    dgv.Rows.RemoveAt(row.Index);
+                }
+            }
+            else //Se as linhas selecionadas forem maior que 0, ou seja se houver linhas selecionadas.
+            {
+                var row = dgv.SelectedRows[0].Cells[0].RowIndex; //A variável row recebe as linhas selecionadas e as celulas.
+                dgv.Rows.RemoveAt(row); //Método que remove as linhas com base nos parâmetros da variável row.
+            }
+        }
+
+        private void AdicionarLinha() //Adiciona uma nova linha à tabela.
+        {
+            //Se as caixas de texto estiverem vazias, retornar uma mensagem.
+            if (string.IsNullOrEmpty(txtNome.Text) || string.IsNullOrEmpty(txtEmail.Text))
+            {
+                MessageBox.Show("Ainda há campos a serem preenchidos!", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else  //Se não, adicionar a linha e limpar as caixas de texto.
+            {
+                dt.Rows.Add(id++, txtNome.Text, txtEmail.Text); //Adiciona a linha com os dados inseridos nas caixas de texto.
+                //Limpa as caixas de texto.
+                txtNome.Text = string.Empty;
+                txtEmail.Text = string.Empty;
+            }
+        }
+
+        private void atualizarLinha() //Atualiza uma linha selecionada já existente na tabela.
+        {
+            if (dgv.SelectedRows.Count > 0) //Se houver linhas selecionadas
+            {
+                try //Tenta executar o que está abaixo.
+                {
+                    //Para cada linha em dgv.SelectedRows, faça.
+                    foreach (DataGridViewRow row in dgv.SelectedRows) //Atualiza cada linha selecionada com o que foi digitado pelo user.
+                    {
+                        //Acrescenta os dados digitados às colunas correspondentes
+                        row.Cells["e-mail"].Value = txtEmail.Text; 
+                        row.Cells["nome"].Value = txtNome.Text;
+
+                        //Limpa as caixas de texto ao atualizar.
+                        txtEmail.Text = string.Empty;
+                        txtNome.Text = string.Empty;
+                    }
+                }
+                catch (Exception ex) //Captura a exceção e retorna em uma mensagem de erro.
+                {
+                    MessageBox.Show(ex.Message, "Ocorreu um erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else if (dgv.SelectedRows.Count == 0) //Se nenhuma linha for selecionada, mostrar a mensagem.
+            {
+                MessageBox.Show("Selecione uma linha primeiro!", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        //Fim da sessão de métodos//
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CarregarTabela(); //Chama o método que faz o datagridview criar as colunas toda vez que o programa é iniciado.
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            AdicionarLinha(); //Chama o método que adiciona as linhas.
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            RemoverLinha(); //Chama o método que remove as linhas.
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            atualizarLinha(); //Chama o método que atualiza as linhas selecionadas.
+        }
+    }
+}
