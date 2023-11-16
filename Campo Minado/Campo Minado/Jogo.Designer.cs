03d6222eff2f8998f674f71109dec90676328bc8
Dan <131774703+patogigantonico@users.noreply.github.com>
2023-11-16 14:06:47 -0300

    Jogo campo minado em c#
    Jogo bem simples, um dos meus primeiros projetos com C#, foi bem desafiador para mim faze-lo.

---
+++ Campo Minado/Campo Minado/Jogo.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Campo_Minado
+{
+    partial class Jogo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Jogo));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.b32 = new System.Windows.Forms.Button();
+            this.b33 = new System.Windows.Forms.Button();
+            this.b31 = new System.Windows.Forms.Button();
+            this.b23 = new System.Windows.Forms.Button();
+            this.b22 = new System.Windows.Forms.Button();
+            this.b21 = new System.Windows.Forms.Button();
+            this.b11 = new System.Windows.Forms.Button();
+            this.b12 = new System.Windows.Forms.Button();
+            this.b13 = new System.Windows.Forms.Button();
+            this.lblPoints = new System.Windows.Forms.Label();
+            this.QuantPontos = new System.Windows.Forms.Label();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.rectangleShape1 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(98, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(75, 55);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 24;
+            this.pictureBox1.TabStop = false;
+            // 
+            // b32
+            // 
+            this.b32.BackColor = System.Drawing.Color.DeepPink;
+            this.b32.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.b32.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.b32.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b32.Location = new System.Drawing.Point(98, 247);
+            this.b32.Name = "b32";
+            this.b32.Size = new System.Drawing.Size(75, 57);
+            this.b32.TabIndex = 22;
+            this.b32.UseVisualStyleBackColor = false;
+            this.b32.Click += new System.EventHandler(this.b32_Click);
+            // 
+            // b33
+            // 
+            this.b33.BackColor = System.Drawing.Color.DeepPink;
+            this.b33.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.b33.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.b33.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b33.Location = new System.Drawing.Point(179, 247);
+            this.b33.Name = "b33";
+            this.b33.Size = new System.Drawing.Size(75, 57);
+            this.b33.TabIndex = 23;
+            this.b33.UseVisualStyleBackColor = false;
+            this.b33.Click += new System.EventHandler(this.b33_Click);
+            // 
+            // b31
+            // 
+            this.b31.BackColor = System.Drawing.Color.DeepPink;
+            this.b31.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.b31.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.b31.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b31.Location = new System.Drawing.Point(17, 247);
+            this.b31.Name = "b31";
+            this.b31.Size = new System.Drawing.Size(75, 57);
+            this.b31.TabIndex = 21;
+            this.b31.UseVisualStyleBackColor = false;
+            this.b31.Click += new System.EventHandler(this.b31_Click);
+            // 
+            // b23
+            // 
+            this.b23.BackColor = System.Drawing.Color.DeepPink;
+            this.b23.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.b23.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.b23.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b23.Location = new System.Drawing.Point(179, 184);
+            this.b23.Name = "b23";
+            this.b23.Size = new System.Drawing.Size(75, 57);
+            this.b23.TabIndex = 20;
+            this.b23.UseVisualStyleBackColor = false;
+            this.b23.Click += new System.EventHandler(this.b23_Click);
+            // 
+            // b22
+            // 
+            this.b22.BackColor = System.Drawing.Color.DeepPink;
+            this.b22.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.b22.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.b22.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b22.Location = new System.Drawing.Point(98, 184);
+            this.b22.Name = "b22";
+            this.b22.Size = new System.Drawing.Size(75, 57);
+            this.b22.TabIndex = 19;
+            this.b22.UseVisualStyleBackColor = false;
+            this.b22.Click += new System.EventHandler(this.b22_Click);
+            // 
+            // b21
+            // 
+            this.b21.BackColor = System.Drawing.Color.DeepPink;
+            this.b21.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.b21.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.b21.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b21.Location = new System.Drawing.Point(17, 184);
+            this.b21.Name = "b21";
+            this.b21.Size = new System.Drawing.Size(75, 57);
+            this.b21.TabIndex = 18;
+            this.b21.UseVisualStyleBackColor = false;
+            this.b21.Click += new System.EventHandler(this.b21_Click);
+            // 
+            // b11
+            // 
+            this.b11.BackColor = System.Drawing.Color.DeepPink;
+            this.b11.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.b11.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.b11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b11.Location = new System.Drawing.Point(17, 121);
+            this.b11.Name = "b11";
+            this.b11.Size = new System.Drawing.Size(75, 57);
+            this.b11.TabIndex = 13;
+            this.b11.UseVisualStyleBackColor = false;
+            this.b11.Click += new System.EventHandler(this.b11_Click);
+            // 
+            // b12
+            // 
+            this.b12.BackColor = System.Drawing.Color.DeepPink;
+            this.b12.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.b12.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.b12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b12.Location = new System.Drawing.Point(98, 121);
+            this.b12.Name = "b12";
+            this.b12.Size = new System.Drawing.Size(75, 57);
+            this.b12.TabIndex = 14;
+            this.b12.UseVisualStyleBackColor = false;
+            this.b12.Click += new System.EventHandler(this.b12_Click);
+            // 
+            // b13
+            // 
+            this.b13.BackColor = System.Drawing.Color.DeepPink;
+            this.b13.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.b13.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.b13.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b13.Location = new System.Drawing.Point(179, 121);
+            this.b13.Name = "b13";
+            this.b13.Size = new System.Drawing.Size(75, 57);
+            this.b13.TabIndex = 17;
+            this.b13.UseVisualStyleBackColor = false;
+            this.b13.Click += new System.EventHandler(this.b13_Click);
+            // 
+            // lblPoints
+            // 
+            this.lblPoints.AutoSize = true;
+            this.lblPoints.BackColor = System.Drawing.Color.Blue;
+            this.lblPoints.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.lblPoints.ForeColor = System.Drawing.Color.White;
+            this.lblPoints.Location = new System.Drawing.Point(25, 36);
+            this.lblPoints.Name = "lblPoints";
+            this.lblPoints.Size = new System.Drawing.Size(52, 17);
+            this.lblPoints.TabIndex = 16;
+            this.lblPoints.Text = "Pontos";
+            // 
+            // QuantPontos
+            // 
+            this.QuantPontos.AutoSize = true;
+            this.QuantPontos.BackColor = System.Drawing.Color.White;
+            this.QuantPontos.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.QuantPontos.Font = new System.Drawing.Font("Impact", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QuantPontos.ForeColor = System.Drawing.Color.Red;
+            this.QuantPontos.Location = new System.Drawing.Point(28, 53);
+            this.QuantPontos.Name = "QuantPontos";
+            this.QuantPontos.Size = new System.Drawing.Size(28, 31);
+            this.QuantPontos.TabIndex = 15;
+            this.QuantPontos.Text = "0";
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.rectangleShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(273, 326);
+            this.shapeContainer1.TabIndex = 25;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // rectangleShape1
+            // 
+            this.rectangleShape1.BackColor = System.Drawing.Color.Blue;
+            this.rectangleShape1.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.rectangleShape1.CornerRadius = 10;
+            this.rectangleShape1.Location = new System.Drawing.Point(17, 18);
+            this.rectangleShape1.Name = "rectangleShape1";
+            this.rectangleShape1.Size = new System.Drawing.Size(238, 90);
+            // 
+            // Jogo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(273, 326);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.b32);
+            this.Controls.Add(this.b33);
+            this.Controls.Add(this.b31);
+            this.Controls.Add(this.b23);
+            this.Controls.Add(this.b22);
+            this.Controls.Add(this.b21);
+            this.Controls.Add(this.b11);
+            this.Controls.Add(this.b12);
+            this.Controls.Add(this.b13);
+            this.Controls.Add(this.lblPoints);
+            this.Controls.Add(this.QuantPontos);
+            this.Controls.Add(this.shapeContainer1);
+            this.MaximizeBox = false;
+            this.Name = "Jogo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Campo Minado";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button b32;
+        private System.Windows.Forms.Button b33;
+        private System.Windows.Forms.Button b31;
+        private System.Windows.Forms.Button b23;
+        private System.Windows.Forms.Button b22;
+        private System.Windows.Forms.Button b21;
+        private System.Windows.Forms.Button b11;
+        private System.Windows.Forms.Button b12;
+        private System.Windows.Forms.Button b13;
+        private System.Windows.Forms.Label lblPoints;
+        private System.Windows.Forms.Label QuantPontos;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape1;
+    }
+}
+
---
+++ Campo Minado/Campo Minado/Jogo.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Campo_Minado
+{
+    public partial class Jogo : Form
+    {
+        //Variáveis.
+        int bomba = 0; //Variável responsável por dizer qual botão vai ser a bomba.
+        int pontos = 0; //Variável responsável por armazenar os pontos.
+        int botao = 8; //Variável que contém os botões máximos que podem ser pressionados.
+        int botoesPressionados = 1; //Variável que contém os botões que foram pressionados.
+        //Esta variavel contém o valor 1 porque o máximo de botões necessários para vencer é 9, ou seja 1 deles é a bomba.
+
+        //Instância da classe random para gerar uma pontuação aleatória.
+        Random rnd = new Random();
+        
+
+        public Jogo()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IniciarJogo(); //Inicia o método assim que o programa é aberto.
+
+            //Remove o destaque ao clicar em um botão
+            b11.TabStop = false;
+            b12.TabStop = false;
+            b13.TabStop = false;
+            b21.TabStop = false;
+            b22.TabStop = false;
+            b23.TabStop = false;
+            b31.TabStop = false;
+            b32.TabStop = false;
+            b33.TabStop = false;
+        }
+
+        private void IniciarJogo() //Inicia o jogo com método GerarBombas.
+        {
+            GerarBombas();
+            VerificarVitória();
+        }
+
+        private void GerarBombas()//Gera as bombas do tabuleiro.
+        {
+            foreach (Control btns in this.Controls) //Para cada controle deste formulário faça.
+            {
+                if (btns is Button && btns != pictureBox1 && btns != lblPoints && btns != QuantPontos) //Se btns for um botão e não for picturebox1, lblpoints, quantpontos faça.
+                {
+                    bomba = new Random().Next(this.Controls.OfType<Button>().Count()); //A variável bomba recebe o valor gerado aleatoriamente dos botões com base na quantidade de botões deste formulário.
+                        switch (bomba) //Atribui o valor de bomba com base no valor da variável bomba.
+                        {
+                            case 0:
+                                b11.Text = "Bomba";
+                                break;
+
+                            case 1:
+                                b12.Text = "Bomba";
+                                break;
+
+                            case 2:
+                                b13.Text = "Bomba";
+                                break;
+
+                            case 3:
+                                b21.Text = "Bomba";
+                                break;
+
+                            case 4:
+                                b22.Text = "Bomba";
+                                break;
+
+                            case 5:
+                                b23.Text = "Bomba";
+                                VerificarVitória();
+                                break;
+
+                            case 6:
+                                b31.Text = "Bomba";
+                                break;
+
+                            case 7:
+                                b32.Text = "Bomba";
+                                break;
+
+                            case 8:
+                                b33.Text = "Bomba";
+                                break;
+
+                            default:
+                                Application.Restart();
+                                break;
+                           
+                        }
+                        break;
+
+                        
+                            
+                        
+                }
+            }
+        }
+
+        private void ReiniciarJogo() //Reinicia o jogo assim que o jogador clica em uma bomba.
+        {
+            Control[] botao = this.Controls.OfType<Control>().Where(b => b != pictureBox1 && b != lblPoints && b != QuantPontos).ToArray();
+
+            foreach (Control botoes in botao) 
+            {
+                if (botoes is Button && botoes != pictureBox1 && botoes != lblPoints && botoes != QuantPontos) 
+                {
+                    GerarBombas(); //Iniciar novamente o jogo.
+
+                    //Limpa o txt dos botões e os ativa novamente.
+                    botoes.Text = "";
+                    botoes.Enabled = true;
+                    botoes.BackColor = Color.DeepPink;
+                    
+
+                    QuantPontos.Text = "0"; //Zera a label.
+                    pontos = 0;//Zera os pontos
+                }
+            }
+        }
+
+        private void VerificarVitória() //Verifica se o jogador pressionou todos os botões exceto o que possui "Bomba". 
+        {
+            Control[] botoes = this.Controls.OfType<Control>().Where(b => b != pictureBox1 && b != lblPoints && b != QuantPontos).ToArray();
+
+            foreach (Control btns in botoes)
+            {
+                if (botao == botoesPressionados) 
+                {
+                    MessageBox.Show("Você venceu!", "Venceu", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    botoesPressionados = 0;
+                    ReiniciarJogo();
+                }
+            }
+        }
+
+        private void PressionarBotao(Button botao) //Evento de click para todos os botões.
+{
+    if (botao.Text == "Bomba") //Se o user clicou no botão com texto bomba, mostra a mensagem e reinicia o jogo.
+    {
+        MessageBox.Show("Você encontrou a bomba!", "Você perdeu", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        ReiniciarJogo();
+    }
+    else
+    {
+        Random pontosAleatórios = new Random();
+        botao.Enabled = false;
+        botao.BackColor = Color.Gray;
+        pontos += pontosAleatórios.Next(50000);
+        QuantPontos.Text = pontos.ToString();
+        VerificarVitória();
+        botoesPressionados++;
+    }
+} 
+
+        
+        //Todos os botões recebem os parâmetros do método PressionarBotão.
+        private void b11_Click(object sender, EventArgs e)
+        {
+            PressionarBotao(b11);
+        }
+
+        private void b12_Click(object sender, EventArgs e)
+        {
+            PressionarBotao(b12);
+        }
+
+        private void b13_Click(object sender, EventArgs e)
+        {
+            PressionarBotao(b13);
+        }
+
+        private void b21_Click(object sender, EventArgs e)
+        {
+            PressionarBotao(b21);
+        }
+
+        private void b22_Click(object sender, EventArgs e)
+        {
+            PressionarBotao(b22);
+        }
+
+        private void b23_Click(object sender, EventArgs e)
+        {
+            PressionarBotao(b23);
+        }
+
+        private void b31_Click(object sender, EventArgs e)
+        {
+            PressionarBotao(b31);
+        }
+
+        private void b32_Click(object sender, EventArgs e)
+        {
+            PressionarBotao(b32);
+        }
+
+        private void b33_Click(object sender, EventArgs e)
+        {
+            PressionarBotao(b33);
+        }
+    }
+}
