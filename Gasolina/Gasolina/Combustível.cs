2d26a6c88413506f4f77fb253543afe4c79995b5
Dan <131774703+patogigantonico@users.noreply.github.com>
2023-11-16 16:55:25 -0300

    Aplicativo que simula uma bomba de gasolina
    Este simples programa feito com c# permite que o usuário saiba quantos litros irá obter e quantos km poderá andar.

---
+++ Gasolina/Gasolina/Combustível.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gasolina
+{
+    class Combustível //Esta classe armazena todos os métodos que fazem o aplicativo funcionar.
+    {
+        //Variáveis.
+        public string NomeVeiculo,tipo;
+        public double KmLitro, ValorLitro, Dinheiro, QtdeLitros, QtdeKM;
+
+        //Métodos
+        public double CalcLitros() 
+        {
+            return Dinheiro / ValorLitro;
+        }
+
+        public double CalcKM() 
+        {
+            return KmLitro * QtdeLitros;
+        }
+
+        public double MostrarResult() 
+        {
+            return QtdeKM = CalcKM();
+        }
+    }
+}
